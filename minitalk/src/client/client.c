b05875477827e025a91a538964c85230f81ff4ba
Nexiliux <nexiliux@gmail.com>
2024-03-25 19:08:16 +0800

    Added Minitalk, Makefile links to libft & ft_printf, added tester in client.c & server.c Fixed printf.c #include printf.h

---
+++ minitalk/src/client/client.c
@@ -0,0 +1,18 @@
+#include "../../libft/libft.h"
+#include "../../printf/includes/ft_printf.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1 || argc > 2){
+	char	*str;
+	str = ft_strdup("Client libft/printf working");
+	ft_printf("%s", str);
+	return (0);
+	}
+	else
+	{
+		ft_printf("%s", argv[1]);
+	}
+	return (0);
+	
+}
